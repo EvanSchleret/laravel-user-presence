26ef90591739900d5f20354e77f273de559a0151
Evan Schleret <evan@schleret.ch>
2025-07-29 15:59:36 +0200

    feat: implement user presence tracker feat

---
+++ src/Middleware/TrackUserActivity.php
@@ -0,0 +1,30 @@
+<?php
+
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrackUserActivity
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $guardName = config('laravel-user-presence.guard');
+
+        if (\App\Http\Middleware\auth()->guard($guardName)->check()) {
+            $user = $request->user();
+
+            $user->last_activity_at = now();
+            $user->timestamps = false;
+            $user->save();
+        }
+
+        return $next($request);
+    }
+}
---
+++ src/Providers/LaravelUserPresenceServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace EvanSchleret\LaraMjml\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelUserPresenceServiceProvider extends ServiceProvider
+{
+    /**
+     * Register LaraMjml service.
+     */
+    public function register(): void
+    {
+        $this->mergeConfig();
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/../../config/laravel-user-presence.php' => config_path('laravel-user-presence.php'),
+            __DIR__.'/../../database/migrations/' => database_path('migrations'),
+        ]);
+    }
+
+    /**
+     * Merge the configuration.
+     */
+    private function mergeConfig(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/laravel-user-presence.php', 'laravel-user-presence'
+        );
+    }
+
+    /**
+     * Publish the configuration.
+     */
+    private function publishConfig(): void
+    {
+        $this->publishes([
+            __DIR__.'/../../config/laravel-user-presence.php' => config_path('laravel-user-presence.php'),
+        ]);
+    }
+}
